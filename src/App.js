55d1f738e8474ff869d8e8a0eeca7f1e3647cb68
learnmakecode <pedroguilherme2200@gmail.com>
2020-08-19 16:20:56 -0300

    1 file modified

--- src/App.js
+++ src/App.js
@@ -3,22 +3,8 @@ import ReactDOM from "react-dom";
 import Estado from "./Estado";
 
 export default function App() {
-  return (
-    <>
-    <div>
-    <Form>
-    <Form.Group controlId="formGroupEmail">
-    <Form.Label>Email address</Form.Label>
-    <Form.Control type="email" placeholder="Enter email" />
-    </Form.Group>
-    <Form.Group controlId="formGroupPassword">
-    <Form.Label>Password</Form.Label>
-    <Form.Control type="password" placeholder="Password" />
-    </Form.Group>
-    </Form>
+  return ( 
     <img src="https://covid.celcoin.com.br/wp-content/uploads/2020/03/celcoin-covid.png" width="350px"/>
     <button onClick={Estado}>Estado</button>
-    </div>
-    </>
   );
 }
